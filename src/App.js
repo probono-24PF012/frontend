21bc0d3ecdf611b1864927802bb2b57e4e1ec9c2
PangKiYeon <pdrkeamy@gmail.com>
2024-07-30 12:07:10 +0900

    FEAT : router 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Main from './pages/main';
+import Chatbot from './pages/chatbot';
+import Signup from './pages/signup';
+import Login from './pages/login';
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Main />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/main" element={<Main />} />
+            <Route path="/chatbot" element={<Chatbot />} />
         </Routes>
     );
 }
